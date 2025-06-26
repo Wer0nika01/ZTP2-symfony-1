3d051a55616b0be57fbf91d973566611c74e2da5
Wer0nika01 <weronika.1.zaczek@student.uj.edu.pl>
2025-06-26 18:54:06 +0200

    Poprawa z php-sniffer

--- app/src/Form/Type/TagType.php
+++ app/src/Form/Type/TagType.php
@@ -16,7 +16,7 @@ class TagType extends AbstractType
     /** * Builds the form.
      *
      * @param FormBuilderInterface $builder
-     * @param array $options */
+     * @param array                $options */
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
